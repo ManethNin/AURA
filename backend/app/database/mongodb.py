894ee0723805c3c603ec43df436cbec7e71b3b2b
ManethNin <kgmninduwara@gmail.com>
2025-12-10 23:36:18 +0530

    database connection

--- backend/app/database/mongodb.py
+++ backend/app/database/mongodb.py
@@ -7,3 +7,23 @@ MongoDB connection manager
 # - Database connection pooling
 # - Health check function
 # - Graceful shutdown
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import settings
+
+
+class MongoDB:
+    client : AsyncIOMotorClient = None
+
+db = MongoDB()
+
+async def connect_db():
+    db.client = AsyncIOMotorClient(settings.MONGODB_URL)
+    print("Connected to MongoDB")
+
+async def close_db():
+    db.client.close()
+    print("Closed MongoDB connection")
+
+def get_database():
+    return db.client[settings.MONGODB_DB_NAME]
